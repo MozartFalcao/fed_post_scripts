a3daf8bfb545e391f2dd75009b63ead6d627ff74
MozartFalcao <mozart.falcao@outlook.com>
2024-04-13 01:09:21 -0300

    vai

--- fedora_post_install/fedora_wks_mzo.sh
+++ fedora_post_install/fedora_wks_mzo.sh
@@ -67,34 +67,29 @@ sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.co
 
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-sudo dnf install brave-browser
+sudo dnf install -y brave-browser 
 
 #Chrome
-sudo dnf install fedora-workstation-repositories
+sudo dnf install -y fedora-workstation-repositories 
 
 sudo dnf config-manager --set-enabled google-chrome
 
-sudo dnf install google-chrome-stable
+sudo dnf -y install google-chrome-stable
 
 #VsCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 dnf check-update
-sudo dnf install code # or code-insiders
-
-#spotify
-sudo dnf install lpf-spotify-client
-lpf update
 
 
 #install from dnf
-sudo dnf install -y zsh firefox steam-devices neovim sqlite3 zsh-autosuggestions zsh-syntax-highlighting setroubleshoot newsboat ffmpeg compat-ffmpeg4 akmod-v4l2loopback yt-dlp guestfs-tools distrobox podman distrobox hugo simple-scan --best --allowerasing
+sudo dnf install -y lpf-spotify-client code zsh firefox steam-devices neovim sqlite3 zsh-autosuggestions zsh-syntax-highlighting setroubleshoot newsboat ffmpeg compat-ffmpeg4 akmod-v4l2loopback yt-dlp guestfs-tools distrobox podman distrobox hugo simple-scan --best --allowerasing
+lpf update
 
 
 
-
-sudo yum install terminator
+sudo yum install -y terminator
 
 # Making Zsh as Default Shell in Fedora
 grep tecmint /etc/passwd
